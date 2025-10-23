faf75c615315240a1a2b8b71d061630cda9b4af9
MattyMar234 <103828861+MattyMar234@users.noreply.github.com>
2025-10-23 12:54:11 +0200

    modifice midi interface
    - Implementazione factory pattern
    - inizio midi over LAN

--- RaspberryPI/src/Midi/midiInterface.py
+++ RaspberryPI/src/Midi/midiInterface.py
@@ -12,49 +12,36 @@ from Utility.multiProcessingWorker import MultiprocessingWorker
 
 class MidiInterface(EventLineInterface, MultiprocessingWorker):
     
-    class Mode(Enum):
-        READ = auto()
-        WRITE = auto()
-
-    @classmethod
-    def getAvailablePorts():
-        midiin = rtmidi.MidiIn()
-        available_ports = midiin.get_ports()
-        return available_ports
     
-    def __init__(self, mode: Mode, interface_name: Optional[str] = None, midi_channel: int = 0, port: Optional[int] = None, sendMidiDataEvent: Optional[Event] = None, reciveMidiDataEvent: Optional[Event] = None) -> None:
+    def __init__(self, interface_name: Optional[str] = None, sendMidiDataEvent: Optional[Event] = None, reciveMidiDataEvent: Optional[Event] = None) -> None:
         EventLineInterface.__init__(self)
         MultiprocessingWorker.__init__(self, interface_name)
 
-        self._port: Optional[int] = None
-        self._mode: MidiInterface.Mode = mode
-        self._midi_channel: int = midi_channel
         self._sendMidiDataEvent: Optional[Event] = sendMidiDataEvent
         self._reciveMidiDataEvent: Optional[Event] = reciveMidiDataEvent
-        
-    def setPort(self, port: int) -> None:
-        self._port = port
-        
+
+        self._loopFunction: Optional[callable] = None
+        self._eventHandleFunction: Optional[callable] = None
+
+    def _setLoopFunctions(self, func: callable):
+        self._loopFunction = func
+
+    def _setEventHandleFunction(self, func: callable):
+        self._eventHandleFunction = func
+
     def setSendMidiDataEvent(self, midiDataEvent: Event) -> None:
         self._sendMidiDataEvent = midiDataEvent
         
     def setReciveMidiDataEvent(self, midiDataEvent: Event) -> None:
         self._reciveMidiDataEvent = midiDataEvent
-    
-    
-    async def async_handleEvent(self, event: EventData):
-        pass
-
-    
-   
-    
         
     def worker_loop_function(self) -> None:
         try:
-            if self._mode == MidiInterface.Mode.READ:
-                self._read_loop()
+            if self._loopFunction is not None:
+                self._loopFunction()
             else:
-                self._write_loop()
+                logging.error(f"Midi interface {self._worker_name} has no loop function set.")
+                raise Exception("No loop function set.")
         except Exception as e:
             raise e
         finally:   
@@ -62,45 +49,7 @@ class MidiInterface(EventLineInterface, MultiprocessingWorker):
             
 
     def handleEvent(self, event: EventData) -> None:
-        pass
+        if self._eventHandleFunction is not None:
+            self._eventHandleFunction(self, event)
         
         
-    def _read_loop(self) -> None:
-
-
-        #se non so che evento mandare aspetto un po
-        while self._sendMidiDataEvent is None or self._port is None:
-            if self._port is None:
-                logging.warning(f"Midi interface {self._worker_name} has no port set, waiting...")
-            if self._sendMidiDataEvent is None:
-                logging.warning(f"Midi interface {self._worker_name} has no midiDataEvent set, waiting...")
-            time.sleep(0.800)
-       
-        logging.info(f"Midi interface {self._worker_name} connected to port {self._port}, starting read loop")
-        midiin = rtmidi.MidiIn()
-        midiin.open_port(self._port)
-        msg = None
-        
-        while self._running.is_set() and self._sendMidiDataEvent is not None:
-            
-            msg = midiin.get_message()
-            
-            if msg is not None:
-                while msg:
-                    self.notifyEvent(EventData(msg, self._sendMidiDataEvent), as_thread=True)
-                    msg = midiin.get_message()
-            else:
-                time.sleep(0.002)
-                
-        
-            
-        
-        
-                
-    def _write_loop(self) -> None:
-        # with mido.open_output(self._port) as outport:
-        #     while self._run_task:
-        #         if len(self._buffer) > 0:
-        #             msg = self._buffer.pop(0)
-        #             outport.send(msg)
-        pass
---
+++ RaspberryPI/src/Midi/midiInterfaceFactory.py
@@ -0,0 +1,161 @@
+from enum import Enum, auto
+import logging
+import time
+from typing import Optional
+import rtmidi
+import socket
+
+
+from Midi.midiInterface import MidiInterface
+from EventLine.eventLine import Event, EventData
+
+class MidiInterfaceFactory():
+
+    class MidiMode(Enum):
+        READ = auto()
+        WRITE = auto()
+
+    class MidiInterfaceType(Enum):
+        USB = auto()
+        LAN = auto()
+
+    @staticmethod
+    def getAvailable_USB_Ports():
+        midiin = rtmidi.MidiIn()
+        available_ports = midiin.get_ports()
+        return available_ports
+    
+    @staticmethod
+    def __usb_read_function(self: MidiInterface, port: int) -> None:
+        #se non so che evento mandare aspetto un po
+        while self._sendMidiDataEvent is None or port is None:
+            if self._port is None:
+                logging.warning(f"Midi interface {self._worker_name} has no port set, waiting...")
+            if self._sendMidiDataEvent is None:
+                logging.warning(f"Midi interface {self._worker_name} has no midiDataEvent set, waiting...")
+            time.sleep(0.800)
+       
+        logging.info(f"Midi interface {self._worker_name} connected to port {port}, starting read loop")
+        
+        try:
+            midiin = rtmidi.MidiIn()
+            midiin.open_port(port)
+            msg = None
+            
+            while self._running.is_set() and self._sendMidiDataEvent is not None:
+                
+                msg = midiin.get_message()
+                
+                if msg is not None:
+                    while msg:
+                        self.notifyEvent(EventData(msg, self._sendMidiDataEvent), as_thread=True)
+                        msg = midiin.get_message()
+                else:
+                    time.sleep(0.002)
+        finally:
+            if midiin is not None:
+                midiin.close_port()
+
+    @staticmethod
+    def __handle_event_function(self: MidiInterface, event: EventData) -> None:
+        self._inputQueue.put(event.data)
+                
+    @staticmethod
+    def __usb_write_function(self: MidiInterface, port: int) -> None:
+        #se non so che evento mandare aspetto un po
+        while self._sendMidiDataEvent is None or port is None:
+            if port is None:
+                logging.warning(f"Midi interface {self._worker_name} has no port set, waiting...")
+            if self._sendMidiDataEvent is None:
+                logging.warning(f"Midi interface {self._worker_name} has no midiDataEvent set, waiting...")
+            time.sleep(0.800)
+
+    
+        try:
+            midiout = rtmidi.MidiOut()
+            midiout.open_port(self._port)
+            msg = None
+            
+            while self._running.is_set() and self._reciveMidiDataEvent is not None:
+
+                if not self._inputQueue.empty():
+                    msg = self._inputQueue.get()
+                else:
+                    time.sleep(0.005)
+                    continue
+
+                while msg is not None:
+                    midiout.send_message(msg) # assumendo che io non abbia modificato il messaggio
+                    msg = self._inputQueue.get()
+                
+        finally:
+            if midiout is not None:
+                midiout.close_port()
+
+    @staticmethod
+    def __lan_read_function(self: MidiInterface, ip_address: str, udp_port: int) -> None:
+        pass
+
+    def __lan_write_function(self: MidiInterface, ip_address: str, udp_port: int) -> None:
+        # --- Setup Socket UDP ---
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self._sock.bind((self.host, self.udp_port))
+        self._sock.settimeout(0.1) # Timeout per non bloccare il loop
+        logging.info(f"[MidiServer] In ascolto su {self.host}:{self.udp_port}")
+
+        while self._running.is_set():
+            try:
+                if not self._inputQueue.empty():
+                    msg = self._inputQueue.get()
+                else:
+                    time.sleep(0.005)
+                    continue
+
+                while msg is not None:
+                    try:
+                        midi_bytes = bytes(msg)
+                        self._sock.sendto(midi_bytes, (self._broadcast_address, self.udp_port))
+                        logging.debug(f"[MidiServer] Inviato in broadcast su {self.udp_port}")
+                                
+                    except Exception as e:
+                        logging.error(f"[MidiServer] Errore nell'invio UDP: {e}")
+  
+                    msg = self._inputQueue.get()
+
+            except Exception as e:
+                logging.error(f"Error in LAN write function of {self._worker_name}: {e}")
+
+     
+
+
+    @staticmethod
+    def create_interface(
+        *,
+        mode: MidiInterface.Mode,
+        connection_type: MidiInterface.ConnectionType,
+        interface_name: Optional[str] = None,
+          sendMidiDataEvent: Optional[Event] = None,
+        reciveMidiDataEvent: Optional[Event] = None,
+
+        usb_port_idx: int = None,
+        ip_address: Optional[str] = None,
+        udp_port: Optional[int] = None
+      
+    ) -> MidiInterface:
+        
+        interface = MidiInterface(interface_name, sendMidiDataEvent, reciveMidiDataEvent)
+
+        if connection_type == MidiInterface.ConnectionType.USB and mode == MidiInterface.Mode.READ:
+            interface._setLoopFunctions(lambda self: MidiInterfaceFactory.__usb_read_function(self, usb_port_idx))
+
+        elif connection_type == MidiInterface.ConnectionType.USB and mode == MidiInterface.Mode.WRITE:
+            interface._setLoopFunctions(lambda self: MidiInterfaceFactory.__usb_write_function(self, usb_port_idx))
+            interface._setEventHandleFunction(lambda self, event: MidiInterfaceFactory.__handle_event_function(self, event))
+
+        elif connection_type == MidiInterface.ConnectionType.LAN and mode == MidiInterface.Mode.READ:
+            interface._setLoopFunctions(lambda self: MidiInterfaceFactory.__lan_read_function(self, ip_address, udp_port))
+
+        elif connection_type == MidiInterface.ConnectionType.LAN and mode == MidiInterface.Mode.WRITE:
+            interface._setLoopFunctions(lambda self: MidiInterfaceFactory.__lan_write_function(self, ip_address, udp_port))
+            interface._setEventHandleFunction(lambda self, event: MidiInterfaceFactory.__handle_event_function(self, event))
